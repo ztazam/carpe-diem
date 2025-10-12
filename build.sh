10548afe9ca2461c687ba30908ddaabfd80cba1c
ztazam <jazelarayan@gmail.com>
2025-10-12 12:40:04 +0200

    debug: check real database connection

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+echo "=== VERIFICANDO CONEXIÓN ==="
+echo "DATABASE_URL: $DATABASE_URL"
+python manage.py shell -c "
+from django.conf import settings
+print('BD REAL:', settings.DATABASES['default']['NAME'])
+"
+echo "============================="
+
+python manage.py migrate
+python manage.py collectstatic --noinput
 set -o errexit
 
 pip install -r requirements.txt
